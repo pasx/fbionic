564473cb62084fa6d4738ee540ce7e40f43ec040
pasx <pascal.guislain@gmail.com>
2018-06-07 12:20:17 +0200

    Updated Firestore

--- api/firestore.ts
+++ api/firestore.ts
@@ -1,20 +1,21 @@
 import * as firebase from "firebase";
+import 'firebase/firestore';
 
 export class Firestore {
     fsto: firebase.firestore.Firestore;
     constructor() {        
         this.fsto = firebase.firestore();
+        const settings = {timestampsInSnapshots: true};
+        this.fsto.settings(settings);
     }
 
-    getAll(collection:string,callback: (data:any)=>void,limit:number):any {
+    getAll(collection:string,callback: (data:any)=>void):any {
         
         this.fsto.collection(collection).get().then((snapshot) =>callback(snapshot));
-        //  {
-        //     callback(snapshot)
-        //     snapshot.forEach((doc) => {
-        //         console.log(`${doc.id} => ${doc.data()}`);
-        //     });
-        // });
 
       };
+
+    getOrdered(collection:string, orderColumn:string, asc:boolean, limit:number, callback: (data:any)=>void){
+        this.fsto.collection(collection).orderBy(orderColumn, asc? "asc":"desc").limit(limit).get().then((snapshot) =>callback(snapshot));
+    }
 }
